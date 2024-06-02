f8111eec74ab950e29925f6ee2486562c50e6250
Andreykhrs <andrey_khrs@mail.ru>
2024-06-02 19:14:26 +0800

    Homework is ready!

---
+++ src/main/java/pro/sky/Homework29/service/OnlineStoreService.java
@@ -0,0 +1,32 @@
+package pro.sky.Homework29.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+import pro.sky.Homework29.exception.OnlineStoreException;
+import pro.sky.Homework29.model.OnlineStore;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+
+@Service
+@SessionScope
+public class OnlineStoreService {
+    List<OnlineStore> list = new ArrayList<>();
+
+    public List<Integer> addId(List<Integer> ID) {
+        if(ID.isEmpty()) {
+            throw new OnlineStoreException();
+        }
+        for (Integer arg : ID) {
+            list.add(new OnlineStore(arg));
+        }
+        return ID;
+    }
+
+    public Collection<OnlineStore> getId() {
+        return Collections.unmodifiableList(list);
+    }
+}
